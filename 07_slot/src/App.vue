ca6b1186728c0c86da7c9efd440abb0979ebfa10
HyeonggyuKang <kin0204@naver.com>
2025-10-23 11:29:59 +0900

    vit기반 vuw 시작

---
+++ 07_slot/src/App.vue
@@ -0,0 +1,28 @@
+<!-- 다른 파일을 불러올 때는 상대경로(./) 또는 src부터의 경로인 절대경로(@)를 사용할 수 있다. -->
+<script setup>
+  // 상대 경로 접근
+  // import MyComponent from './components/01_componet/MyComponent.vue';
+
+  // 절대 경로 (src 경로 부터 시작 @ <- src 위치 가 됨)
+  import MyComponent from '@/components/01_componet/MyComponent.vue';
+</script>
+    
+<template>
+  <header>
+    <div class="redColor">
+      <MyComponent></MyComponent>
+    </div>
+  </header>
+  <main>
+
+  </main>
+
+</template>
+
+
+
+<style scoped>
+  .redColor {
+    color : red;
+  }
+</style>
