c65194e28f93f29172552ff6a7a665c00f9447ca
hyeongKang <kin0204@naver.com>
2025-10-23 22:31:49 +0900

    주석으로 route 설명 추가

--- 09_router/src/App.vue
+++ 09_router/src/App.vue
@@ -6,13 +6,13 @@
   <header>
     <h2>basic router</h2>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/">Home</RouterLink> <!--a 테그와 비슷하게 페이지를 이동 시키기 위한 요소(선택하면 해당 컴포넌트로 routing)-->
       <RouterLink to="/pathvariable/1">PathVariable</RouterLink>
       <RouterLink to="/querystring?name=홍길동$age=20">QueryString</RouterLink>
     </nav>
   </header>
   <main>
-    <RouterView/>
+    <RouterView/> <!--link된 component가 그려지는 위치-->
   </main>
 </template>
 
